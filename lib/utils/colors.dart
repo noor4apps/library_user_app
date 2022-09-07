5e6219e3375aa90c70a21d4add7aa9ed1fd27aeb
MuhammadNoor <noor4apps@gmail.com>
2022-09-08 02:26:52 +0300

    added the pictures and the colors file

---
+++ lib/utils/colors.dart
@@ -0,0 +1,31 @@
+import 'dart:ui';
+
+class AppColors {
+  static final Color textPrimary = const Color(0xFF333333);
+  static final Color textSecondary = const Color(0xFFe59285);
+
+  static final Color bg = const Color(0xFFffffff);
+  static final Color meanBar = const Color(0xFF191919);
+  static final Color footer = const Color(0xFFebebeb);
+
+  static final Color iconPrimary = const Color(0xFFbb7152);
+  static final Color iconSecondary = const Color(0xFFf1f1f1);
+  static final Color button = const Color(0xFF494949);
+
+  static final Color badgeDark = const Color(0xFF343a40db);
+  static final Color badgeInfo = const Color(0xFF17a2b8);
+  static final Color badgeLight = const Color(0xFFe9ecef);
+
+  static final Color alertSuccessBg = const Color(0xFFd4edda);
+  static final Color alertSuccessTex = const Color(0xFF155724);
+
+  static final Color alertInfoBg = const Color(0xFFd1ecf1);
+  static final Color alertInfoTex = const Color(0xFF0c5460);
+
+  static final Color alertDangerBg = const Color(0xFFf8d7da);
+  static final Color alertDangerTex = const Color(0xFF721c24);
+
+  static final Color submitting = const Color(0xFFFDB45C);
+  static final Color checkout = const Color(0xFFF7464A);
+  static final Color returned = const Color(0xFF46BFBD);
+}
